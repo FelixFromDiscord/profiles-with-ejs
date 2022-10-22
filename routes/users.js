c60599eb76b38630f854cc628887c264450cf1f5
FelixFromDiscord <68750194+FelixFromDiscord@users.noreply.github.com>
2022-10-23 01:14:09 +0300

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 router.get('/:id', function(req, res, next) {
-  let fileData = fs.readFileSync(path.join(__dirname, "..", "data\\users.json"));
+  let fileData = fs.readFileSync(path.join(__dirname, "..", "data/users.json"));
   const data = JSON.parse(fileData.toString());
   let options = data[req.params.id];
   if (options === undefined) {
