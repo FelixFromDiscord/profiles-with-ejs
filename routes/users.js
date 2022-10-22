cbb7087efe036483ac9b0405124f1b1fe35adfca
FelixFromDiscord <68750194+FelixFromDiscord@users.noreply.github.com>
2022-10-23 01:08:54 +0300

    initial

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+router.get('/:id', function(req, res, next) {
+  let fileData = fs.readFileSync(path.join(__dirname, "..", "data\\users.json"));
+  const data = JSON.parse(fileData.toString());
+  let options = data[req.params.id];
+  if (options === undefined) {
+    options = { userNotFound: true };
+  } else {
+    options.userNotFound = false;
+  }
+  res.render('user', options);
+});
+
+module.exports = router;
